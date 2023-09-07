a7c9b916bef2e19ab5da6fae176e2cd34aab4db0
lourdeskyle <lourdesmkc@gmail.com>
2023-09-07 12:03:02 -0700

    Registration Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,84 +1,116 @@
 <x-app-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<div class=" flex flex-row sm:px-6 lg:-ml-9">
+        <div class="bg-gold w-[315px] h-[1024px] flex justify-start flex-col py-5 px-2">
+            <div class="self-center mr-5 bg-dirtywhite rounded-full w-[140px] h-[134px] sm:-my-px sm:ml-10 sm:flex px-14">
+                   <!-- <img src="images/logo.png" alt=""  style="width: 100px; height:100px;"> -->
+            </div>
+            <div class="-mt-[112px] sm:ml-[112px] lg:ml-[112px]">
+                <img src="images/logo.png" alt=""  style="width: 100px; height:100px;">
+            </div>
+            <div class="mt-20 self-center">
+                <p class="font-poppins text-2xl underline underline-offset-8">Isabella Rodriguez</p>
+                @role('Admin')
+                <p class="font-poppins text-2xl text-center text-darkblue">ADMIN</p>
+                @endrole
+            </div>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="firstName" :value="__('First Name')" />
-            <x-text-input id="firstName" class="block mt-1 w-full" type="text" name="firstName" :value="old('firstName')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('firstName')" class="mt-2" />
-        </div>
+            <div class="mt-24 self-center bg-gold border-y-2 border-dirtywhite shadow-md px-4 py-2 hover:shadow-inner">
+                <a href="" class="font-poppins text-2xl">User Accounts</a>
+            </div>
 
-        <div>
-            <x-input-label for="middleName" :value="__('Middle Name')" />
-            <x-text-input id="middleName" class="block mt-1 w-full" type="text" name="middleName" :value="old('middleName')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('middleName')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="lastName" :value="__('Last Name')" />
-            <x-text-input id="lastName" class="block mt-1 w-full" type="text" name="lastName" :value="old('lastName')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('lastName')" class="mt-2" />
-        </div>
-        
-        <div class="mt-4">
-            <x-input-label for="dateOfBirth" :value="__('Date of Birth')" />
-            <x-text-input id="dateOfBirth" class="block mt-1 w-full" type="date" name="dateOfBirth" :value="old('dateOfBirth')" required autocomplete="on" />
-            <x-input-error :messages="$errors->get('dateOfBirth')" class="mt-2" />
-        </div>
-
-        <div>
-        <x-input-label for="userRole" :value="__('User Role')" />
-                <select id="userRole" class="block mt-1 w-full" name="userRole" :value="old('userRole')" required autofocus>
-                    @foreach($roles as $role)
-                    <option value="{{$role->name}}">{{$role->name}}</option>
-                    @endforeach
-                </select>
+            <div class="static mt-24 self-center bg-gold border-y-2 border-dirtywhite shadow-md px-8 py-2 hover:shadow-inner">
+                <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('LOGOUT') }}
+                        </a>
+                </form>
+            </div>
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="Email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-        <div class="mt-4">
-            <x-input-label for="contactNumber" :value="__('Contact Number')" />
-            <x-text-input id="contactNumber" class="block mt-1 w-full" type="text" name="contactNumber" :value="old('contactNumber')" required autocomplete="on" />
-            <x-input-error :messages="$errors->get('contactNumber')" class="mt-2" />
-        </div>
+        <div class="flex flex-col items-center w-max sm:px-6 lg:ml-[15rem] bg-dirtywhite border-l-blue-950 shadow-md h-max mt-[15rem]">
+                <p class="font-poppins mb-4 text-2xl underline underline-offset-8">Add Account</p>
+            <form method="POST" action="{{ route('register') }}" class="flex flex-col flex-wrap w-[800px] h-[300px]">
+                @csrf
+
+                <!-- Name -->
+                <div class="mt-4 px-4">
+                    <x-input-label for="firstName" :value="__('First Name')" />
+                    <x-text-input id="firstName" class="block mt-1 w-full" type="text" name="firstName" :value="old('firstName')" required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('firstName')" class="mt-2" />
+                </div>
+
+                <div class="mt-4  px-4">
+                    <x-input-label for="middleName" :value="__('Middle Name')" />
+                    <x-text-input id="middleName" class="block mt-1 w-full" type="text" name="middleName" :value="old('middleName')" required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('middleName')" class="mt-2" />
+                </div>
+
+                <div class="mt-4  px-4">
+                    <x-input-label for="lastName" :value="__('Last Name')" />
+                    <x-text-input id="lastName" class="block mt-1 w-full" type="text" name="lastName" :value="old('lastName')" required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('lastName')" class="mt-2" />
+                </div>
+                
+                <div class="mt-4 px-4 ">
+                    <x-input-label for="dateOfBirth" :value="__('Date of Birth')" />
+                    <x-text-input id="dateOfBirth" class="block mt-1 w-full" type="date" name="dateOfBirth" :value="old('dateOfBirth')" required autocomplete="on" />
+                    <x-input-error :messages="$errors->get('dateOfBirth')" class="mt-2" />
+                </div>
+
+                <div class="mt-4 px-4 ">
+                    <x-input-label for="contactNumber" :value="__('Contact Number')" />
+                    <x-text-input id="contactNumber" class="block mt-1 w-full" type="text" name="contactNumber" :value="old('contactNumber')" required autocomplete="on" />
+                    <x-input-error :messages="$errors->get('contactNumber')" class="mt-2" />
+                </div>
+
+                <!-- Email Address -->
+                <div class="mt-4 px-4">
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="Email" name="email" :value="old('email')" required autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+                
+                <div class="mt-4 px-4 ">
+                <x-input-label for="userRole" :value="__('User Role')" />
+                        <select id="userRole" class="block mt-1 w-full" name="userRole" :value="old('userRole')" required autofocus>
+                            @foreach($roles as $role)
+                            <option value="{{$role->name}}">{{$role->name}}</option>
+                            @endforeach
+                        </select>
+                </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <!-- <div class="mt-4 px-4 ">
+                <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div> -->
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                <!-- <div class="mt-4 px-4 ">
+                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                    <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                    type="password"
+                                    name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div> -->
+
+                <div class="flex justify-end mt-4 px-4">
+                    <x-primary-button class="">
+                        {{ __('Register') }}
+                    </x-primary-button>
+                </div>
+            </form>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,25 +5,127 @@
         </h2>
     </x-slot>
 
-    <div class="sm:px-6 lg:-ml-9">
+    <div class=" flex flex-row sm:px-6 lg:-ml-9">
         <div class="bg-gold w-[315px] h-[1024px] flex justify-start flex-col py-5 px-2">
-            <div class="self-center mr-5 mt-5 bg-dirtywhite rounded-full w-[140px] h-[134px] sm:-my-px sm:ml-10 sm:flex px-14">
+            <div class="self-center mr-5 bg-dirtywhite rounded-full w-[140px] h-[134px] sm:-my-px sm:ml-10 sm:flex px-14">
                    <!-- <img src="images/logo.png" alt=""  style="width: 100px; height:100px;"> -->
             </div>
-            <div class="absolute ml-[112px] mt-4">
+            <div class="-mt-[112px] sm:ml-[112px] lg:ml-[112px]">
                 <img src="images/logo.png" alt=""  style="width: 100px; height:100px;">
             </div>
-            
-        </div>
-        <!-- <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Create Account</a> -->
-        <form method="POST" action="{{ route('logout') }}">
-                    @csrf
+            <div class="mt-20 self-center">
+                <p class="font-poppins text-2xl underline underline-offset-8">Isabella Rodriguez</p>
+                @role('Admin')
+                <p class="font-poppins text-2xl text-center text-darkblue">ADMIN</p>
+                @endrole
+            </div>
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
+            <div class="mt-24 self-center bg-gold border-y-2 border-dirtywhite shadow-md px-4 py-2 hover:shadow-inner">
+                <a href="" class="font-poppins text-2xl">User Accounts</a>
+            </div>
+
+            <div class="static mt-24 self-center bg-gold border-y-2 border-dirtywhite shadow-md px-8 py-2 hover:shadow-inner">
+                <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('LOGOUT') }}
+                        </a>
                 </form>
+            </div>
+        </div>
+        <div class="flex flex-col mt-20 w-max sm:px-6 lg:ml-9">
+            <div class=" bg-gold w-max self-end shadow-inner px-4 py-2 hover:shadow-md">
+                <a href="{{ route('register') }}" class="text-center text-sm text-darkblue font-semibold hover:text-dirtywhite">CREATE ACCOUNT</a>
+            </div>
+            <!-- <div class=" bg-dirtywhite w-full h-96 mt-4 border-2 border-darkblue shadow-md relative overflow-x-auto"> -->
+                <!-- <div class="bg-darkblue w-full h-14 border-2 border-dirtywhite-600 shadow-inner flex justify-between items-center px-4">
+                    <p class="font-poppins text-base text-dirtywhite font-semibold">ID</p>
+                    <p class="font-poppins text-base text-dirtywhite font-semibold">NAME</p>
+                    <p class="font-poppins text-base text-dirtywhite font-semibold">EMAIL</p>
+                    <p class="font-poppins text-base text-dirtywhite font-semibold">CONTACT NUMBER</p>
+                    <p class="font-poppins text-base text-dirtywhite font-semibold">ACTION</p>
+                </div> -->  
+            <!-- </div> -->
+
+            <div class="relative overflow-x-auto bg-dirtywhite border-2 border-darkblue shadow-md w-[1400px] h-max mt-4 ml-5">
+                <table class="w-full  text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="bg-darkblue border-2 border-dirtywhite-600 shadow-inner text-md text-dirtywhite uppercase">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                ID
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                NAME
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                EMAIL
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                CONTACT NUMBER
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                ACTION
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                               EMP001
+                            </th>
+                            <td class="px-6 py-4">
+                                Kim Mingyu
+                            </td>
+                            <td class="px-6 py-4">
+                                mingyu@bdrs.com
+                            </td>
+                            <td class="px-6 py-4">
+                                2999
+                            </td>
+                            <td class="px-6 py-4">
+                                UPDATE | ARCHIVE
+                            </td>
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                EMP002
+                            </th>
+                            <td class="px-6 py-4">
+                                Lalisa Manoban
+                            </td>
+                            <td class="px-6 py-4">
+                                lalisa@bdrs.com
+                            </td>
+                            <td class="px-6 py-4">
+                                1999
+                            </td>
+                            <td class="px-6 py-4">
+                                UPDATE | ARCHIVE
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-gray-800">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                EMP003
+                            </th>
+                            <td class="px-6 py-4">
+                                Jeon Wonwoo
+                            </td>
+                            <td class="px-6 py-4">
+                                wonwoo@gmail.com
+                            </td>
+                            <td class="px-6 py-4">
+                                9987
+                            </td>
+                            <td class="px-6 py-4">
+                                UPDATE | ARCHIVE
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </div>
 </x-app-layout>
